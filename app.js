8e591c31c6fe1de32bd174f2d2410ce15d680f09
Elias Dorneles <elias.dorneles@hey.com>
2021-12-24 23:23:57 +0100

    add working basic version of permission wheel

--- app.js
+++ app.js
@@ -27,21 +27,83 @@ const wheel = document.getElementById('wheel')
 const radius = 310
 const n = 16
 
-Array.from({length: 10}, (_, x) => {
+const PERMISSIONS = [
+  // first quadrant - me and my feelings
+  'Joy',
+  'Anger',
+  'Fear',
+  'Sadness',
+
+  // second quadrant - me and the world
+  'Succeed',
+  'Think',
+  'Know',
+  'Grow up',
+
+  // 3rd quadrant - me and others
+  'Be a Child',
+  'Be Close',
+  'Belong',
+  'Trust',
+
+  // 4th quadrant - me and myself
+  'Pleasure',
+  'Self',
+  'Health',
+  'Exist',
+]
+
+const NUM_LEVELS = 10
+
+Array.from({length: NUM_LEVELS}, (_, i) => {
+  const level = 10 - i
   const arc = d3.arc()
       .innerRadius(0)
-      .outerRadius(31 * x)
+      .outerRadius(30 * level)
       .startAngle(([startAngle, endAngle]) => startAngle)
       .endAngle(([startAngle, endAngle]) => endAngle)
 
+  const n = PERMISSIONS.length
   Array.from({length: n}, (_, i) => {
+    const angle = [i / n * 2 * Math.PI, (i + 1) / n * 2 * Math.PI]
     appendTo(wheel, 'path', {
-      stroke: '#778899',
+      stroke: '#cccccc',
       'stroke-width': '2px',
       // fill: d3.interpolateRainbow(i / n),
+      class: `permission permission_${i} level_${level}`,
+      data: JSON.stringify({
+        "permission": i,
+        "level": level,
+      }),
       fill: 'transparent',
-      d: arc([i / n * 2 * Math.PI, (i + 1) / n * 2 * Math.PI]),
+      d: arc(angle),
       cursor: 'pointer',
     })
   })
 })
+
+const arcWords = d3.arc()
+    .innerRadius(340)
+    .outerRadius(340)
+    .startAngle(([startAngle, endAngle]) => startAngle)
+    .endAngle(([startAngle, endAngle]) => endAngle)
+
+PERMISSIONS.forEach((permissionText, i) => {
+  const n = PERMISSIONS.length
+  const angle = [i / n * 2 * Math.PI, (i + 1) / n * 2 * Math.PI]
+  const [xPos, yPos] = arcWords.centroid(angle)
+  appendTo(wheel, 'text', {
+    fill: '#223344',
+    transform: `translate(${xPos - 30}, ${yPos})`,
+  }, permissionText)
+})
+
+wheel.querySelectorAll('.permission').forEach( sectorElem =>
+  sectorElem.addEventListener('click', () => {
+    const data = JSON.parse(sectorElem.getAttribute('data'))
+    wheel.querySelectorAll(`.permission_${data.permission}`).forEach(
+      el => el.setAttribute('fill', 'transparent')
+    )
+    sectorElem.setAttribute('fill', '#4444aa')
+  })
+)
