4a9481e0461f2ba6d685994ac27414de4ce692d6
Elias Dorneles <elias.dorneles@hey.com>
2021-12-25 14:45:31 +0100

    cleanup: use d3 select and append methods

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-// helper functions stolen from: https://stackoverflow.com/a/9727850/149872
-function appendTo(node, name, attrs, text) {
-  var p,
-    ns = appendTo.ns,
-    svg = node,
-    doc = node.ownerDocument
-  if (!ns) {
-    // cache namespaces by prefix once
-    while (svg && svg.tagName != "svg") svg = svg.parentNode
-    ns = appendTo.ns = { svg: svg.namespaceURI }
-    for (var a = svg.attributes, i = a.length; i--; ) {
-      if (a[i].namespaceURI) ns[a[i].localName] = a[i].nodeValue
-    }
-  }
-  var el = doc.createElementNS(ns.svg, name)
-  for (var attr in attrs) {
-    if (!attrs.hasOwnProperty(attr)) continue
-    if (!(p = attr.split(":"))[1]) el.setAttribute(attr, attrs[attr])
-    else el.setAttributeNS(ns[p[0]] || null, p[1], attrs[attr])
-  }
-  if (text) el.appendChild(doc.createTextNode(text))
-  return node.appendChild(el)
-}
-
-function clear(node) {
-  while (node.lastChild) node.removeChild(node.lastChild)
-}
-
 const wheel = document.getElementById("wheel")
 
 const radius = 310
@@ -71,19 +43,21 @@ Array.from({ length: NUM_LEVELS }, (_, i) => {
   const n = PERMISSIONS.length
   Array.from({ length: n }, (_, i) => {
     const angle = [(i / n) * 2 * Math.PI, ((i + 1) / n) * 2 * Math.PI]
-    appendTo(wheel, "path", {
-      stroke: "#cccccc",
-      "stroke-width": "2px",
-      // fill: d3.interpolateRainbow(i / n),
-      class: `permission permission_${i} level_${level}`,
-      data: JSON.stringify({
-        permission: i,
-        level: level,
-      }),
-      fill: "transparent",
-      d: arc(angle),
-      cursor: "pointer",
-    })
+    d3.select(wheel)
+      .append("path")
+      .attr("stroke", "#bbbbbb")
+      .attr("stroke-width", "2px")
+      .attr("cursor", "pointer")
+      .attr("fill", "transparent")
+      .attr("d", arc(angle))
+      .classed(`permission permission_${i} level_${level}`, true)
+      .attr(
+        "data",
+        JSON.stringify({
+          permission: i,
+          level: level,
+        }),
+      )
   })
 })
 
@@ -98,16 +72,12 @@ PERMISSIONS.forEach((permissionText, i) => {
   const n = PERMISSIONS.length
   const angle = [(i / n) * 2 * Math.PI, ((i + 1) / n) * 2 * Math.PI]
   const [xPos, yPos] = arcWords.centroid(angle)
-  appendTo(
-    wheel,
-    "text",
-    {
-      fill: "#223344",
-      "font-size": "1.5em",
-      transform: `translate(${xPos - 30}, ${yPos})`,
-    },
-    permissionText,
-  )
+  d3.select(wheel)
+    .append("text")
+    .attr("fill", "#223344")
+    .attr("font-size", "1.5em")
+    .attr("transform", `translate(${xPos - 30}, ${yPos})`)
+    .text(permissionText)
 })
 
 wheel.querySelectorAll(".permission").forEach((sectorElem) => {
@@ -116,7 +86,9 @@ wheel.querySelectorAll(".permission").forEach((sectorElem) => {
     wheel
       .querySelectorAll(`.permission_${data.permission}`)
       .forEach((el) => el.setAttribute("fill", "transparent"))
-    sectorElem.setAttribute("fill", "#4444aa")
+    const color = "#4444aa"
+    // const color = d3.interpolateRainbow(data.permission / PERMISSIONS.length - 0.5)
+    sectorElem.setAttribute("fill", color)
   }
   sectorElem.addEventListener("click", resetPermissionSector)
 })
