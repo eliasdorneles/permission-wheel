6d6abc42577d1fce6d4b2e6f7bcbaa4db08fb8a4
Elias Dorneles <elias.dorneles@hey.com>
2021-12-25 17:02:18 +0100

    add quadrant label boxes

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+// stolen from: https://stackoverflow.com/a/28553412/149872
+function wrap(text, width) {
+  text.each(function () {
+    var text = d3.select(this),
+      words = text.text().split(/\s+/).reverse(),
+      word,
+      line = [],
+      lineNumber = 0,
+      lineHeight = 1.1, // ems
+      y = text.attr("y"),
+      dy = parseFloat(text.attr("dy")) || 0,
+      tspan = text
+        .text(null)
+        .append("tspan")
+        .attr("x", 0)
+        .attr("y", y)
+        .attr("dy", dy + "em")
+    while ((word = words.pop())) {
+      line.push(word)
+      tspan.text(line.join(" "))
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop()
+        tspan.text(line.join(" "))
+        line = [word]
+        tspan = text
+          .append("tspan")
+          .attr("x", 0)
+          .attr("y", y)
+          .attr("dy", ++lineNumber * lineHeight + dy + "em")
+          .text(word)
+      }
+    }
+  })
+}
+
 const svgBox = document.getElementById("svgBox")
 
 const radius = 310
@@ -131,6 +166,30 @@ d3.select(svgBox)
   .attr("fill", "#223344")
   .attr("transform", `rotate(180) translate(0, -${arrowRadius})`)
   .attr("d", d3.symbol(d3.symbolTriangle, 300)())
+
+// draw quadrant labels
+const drawTextBox = (textContent, xPos, yPos) => {
+  const textWidth = 120
+  const g = d3.select(svgBox).append("g")
+  g.append("rect")
+    .attr("stroke", "#223344")
+    .attr("fill", "transparent")
+    .attr("x", xPos)
+    .attr("y", yPos)
+    .attr("width", textWidth)
+    .attr("height", 70)
+  var text = g
+    .append("text")
+    .attr("transform", `translate(${xPos + 15}, ${yPos + 30})`)
+    .attr("font-size", "1.2em")
+    .text(textContent)
+    .call(wrap, textWidth - 15)
+}
+
+drawTextBox("Me and my feelings", 250, -350)
+drawTextBox("Me and myself", -380, -350)
+drawTextBox("Me and others", -380, 250)
+drawTextBox("Me and the world", 250, 250)
 
 // setup events
 svgBox.querySelectorAll(".permission").forEach((sectorElem) => {
